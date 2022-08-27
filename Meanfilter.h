22baa56fbfe602f8b1848c4005f49860d8c78b72
罗毓康 <11487204+luoyukang@user.noreply.gitee.com>
2022-08-27 15:47:22 +0000

    均值滤波
    Signed-off-by: 罗毓康 <11487204+luoyukang@user.noreply.gitee.com>

---
+++ Meanfilter.h
@@ -0,0 +1,13 @@
+#ifndef MEANFILTER_H
+#define MEANFILTER_H
+
+#include "Filter.h"
+class MeanFilter : public Filter
+{
+public:
+    MeanFilter(int size);
+    virtual ~MeanFilter();
+    virtual Matrix Filtering(const Matrix& input); 
+};
+
+#endif
